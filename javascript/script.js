39dc316b0d99f2faee970bab0bacc785d0d10675
dameeta <dameeta835@gmail.com>
2025-07-03 18:44:20 +0530

    javascript day1

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+ function hello() {
+
+    
+ var i=8;
+ console.log(typeof i); // "number"
+ i="hi";
+ console.log(typeof i); // "string"
+    // Uncomment the lines below to see the output in the console
+    
+    //   alert("Hello, this is an internal JavaScript demo!");
+    //   console.log("this will be printed on the console");
+    //   console.warn("this is a warning message");
+    //   console.error("This is an error");
+    //   console.info("This is an info message");
+    //   console.debug("This is a debug message");
+    //   console.log("This is a log message");
+    // 
+    }
---
+++ javascript/varibles.js
@@ -0,0 +1,24 @@
+<script>
+var i = 90
+console.log(typeof i) // "number"
+let j = 'Narayana'
+const balance = 1000.5
+console.log(typeof j) // "string"
+console.log(typeof balance)
+
+function test_variable () {
+  var num1 = 'hello'
+  let num2 = 122;
+  const num3 = 123.45;
+  console.log(typeof i) // "string"
+  j = 444
+  console.log(typeof j) // "number"
+  balance = 899.56
+  console.log(typeof balance)
+  console.log(balance)
+}
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+</script>
