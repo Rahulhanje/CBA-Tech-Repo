2b716ed47bf7b6ea3e65476780275519c35f96e6
dameeta <dameeta835@gmail.com>
2025-07-04 18:42:42 +0530

    day2

---
+++ javascript/defltParm.js
@@ -0,0 +1,6 @@
+function addition(a, b) {
+    if (b === void 0) { b = 3; }
+    return a + b;
+}
+console.log(addition(5));
+console.log(addition(5, 4));
---
+++ javascript/defltParm.ts
@@ -0,0 +1,7 @@
+function addition(a, b=3){
+  return a + b;
+}
+
+console.log(addition(5));
+console.log(addition(5,4));
+
