b497efc3fdd4f5e1cfd8c83a612c5919021823ef
Dameeta <85731431+dameeta@users.noreply.github.com>
2025-07-31 18:45:51 +0530

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a company dedicated to providing the best services to our customers.</p>
+      <p>Our team is composed of experienced professionals who are passionate about their work.</p>
+    </div>
+  );
+} 
+export default AboutUs;
--- App.jsx
+++ App.jsx
@@ -1,19 +1,32 @@
-import ClassComponent from './components/ClassComponent'
-import FunctionalComponent from './components/FunctionalComponent'
-import PropsComponent from './components/PropsComponent';
 
-function App() {
+import './App.css'
+import ContactForm from './Pages/ContactForm'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home'
+import AboutUs from './Pages/AboutUs'
+
+
+const App=() =>  {
 
   return (
-    <>
-        <ClassComponent/>
-        <hr />
-        <FunctionalComponent/>
-        <hr />
-        <PropsComponent name="Swami Narayan" />
-        <hr />
-    </>
-  );
+        <Router>
+          <div>
+        <nav>
+          <Link to="/">Home</Link>| {"  "}
+          <Link to="/contact">Contact</Link>| {"  "}
+          <Link to="/about">About</Link>| {"  "}  
+
+          </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<ContactForm/>} />
+          <Route path="/about" element={<AboutUs/>} />
+
+        </Routes>
+      </div>
+    </Router>
+    
+  )
 }
 
 export default App
